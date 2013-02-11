8e5622785e9e3c63a0fb9f4a6625c05bcbb48c87
Stefan Obermeier <post@stefan-obermeier.net>
2013-02-11 18:09:35 +0100

    Read Model from file and test basic graph query.

--- play-dcep-distributedetalis/src/test/java/eu/play_project/dcep/distributedetalis/test/EcConnectinManagerLocalTest.java
+++ play-dcep-distributedetalis/src/test/java/eu/play_project/dcep/distributedetalis/test/EcConnectinManagerLocalTest.java
@@ -2,96 +2,75 @@ package eu.play_project.dcep.distributedetalis.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
 import org.junit.Test;
+import org.ontoware.rdf2go.exception.ModelRuntimeException;
+import org.ontoware.rdf2go.exception.SyntaxNotSupportedException;
+import org.ontoware.rdf2go.model.ModelSet;
+import org.ontoware.rdf2go.model.Syntax;
 
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.query.Dataset;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.sparql.core.Quad;
 import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
+import eu.play_project.dcep.distributedetalis.join.ResultRegistry;
+import eu.play_project.play_commons.eventtypes.EventHelpers;
+
+
+import fr.inria.eventcloud.api.Quadruple;
 import fr.inria.eventcloud.api.responses.SparqlSelectResponse;
 import fr.inria.eventcloud.api.wrappers.ResultSetWrapper;
 
 /**
  * Tests of the local version of EcConnectionManager.
+ * 
  * @author Stefan Obermeier
- *
+ * 
  */
 public class EcConnectinManagerLocalTest {
 
 	/**
-	 * Create  Jena model and query some data.
+	 * Read Model from file and test basic graph query.
 	 */
 	@Test
-	public void generateQueryResult() {
-		
-		Model model = ModelFactory.createDefaultModel();
-
-		Resource modelTests1 = model
-				.createResource("http://play-project.eu/modelTest");
-		modelTests1.addProperty(VCARD.FN, "1234");
-
-		Resource modelTests2 = model
-				.createResource("http://play-project.eu/modelTest");
-		modelTests2.addProperty(VCARD.FN, "4321");
-
-		// Query data from model
-		Query query = QueryFactory.create("SELECT ?O WHERE {?S ?P ?O}");
-		QueryExecution qexec = QueryExecutionFactory.create(query, model);
-
-		SparqlSelectResponse result;
-		try {
-			ResultSet results = qexec.execSelect();
-
-			// Put result in PLAY result wrapper.
-			ResultSetWrapper dataIn = new ResultSetWrapper(results);
-			result = new SparqlSelectResponse(1, 1, 1, 1, dataIn);
-		} finally {
-			qexec.close();
-		}
-
-		assertTrue(result.getResult().next().get("O").toString().equals("4321"));
-		assertTrue(result.getResult().next().get("O").toString().equals("1234"));
-		
-//		//Print results. (Copy form join-engine)
-//		while (result.getResult().hasNext()) {
-//			QuerySolution data = result.getResult().next();
-//			Iterator<String> iter1 = data.varNames();
-//
-//			// Print variable name and value.
-//			while (iter1.hasNext()) {
-//				String next = iter1.next();
-//				System.out.println(next + "\t" + data.get(next));
-//			}
-//		}
-
-	}
-	
-	@Test
-	public void readModelFromFile(){
+	public void readModelFromFile() throws SyntaxNotSupportedException, ModelRuntimeException, IOException {
 		// create an empty model
-		 Model model = ModelFactory.createDefaultModel();
-		 
-		 String inputFileName = "Example-historical-RDF-model.rdf";
-		 // use the FileManager to find the input file
-		 InputStream in = FileManager.get().open( inputFileName );
+		ModelSet rdf = EventHelpers.createEmptyModelSet();
+
+		String inputFileName = "Example-historical-RDF-model.trig";
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream(inputFileName);
 		if (in == null) {
-		    throw new IllegalArgumentException("File: " + inputFileName + " not found");
+			throw new IllegalArgumentException("File: " + inputFileName+ " not found");
 		}
 
-		model.read(in, null);
+		rdf.readFrom(in, Syntax.Trig);
 
 		// Query data from model
-		Query query = QueryFactory.create("SELECT ?O WHERE {?S ?P ?O}");
-		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		Query query = QueryFactory.create("SELECT ?O WHERE { GRAPH ?id {?S <http://events.event-processing.org/types/screenName> \"roland.stuehmer\"." +
+																		"?S <http://events.event-processing.org/types/twitterName> ?O}}");
+		Dataset jena = (Dataset) rdf.getUnderlyingModelSetImplementation();
+		
+		QueryExecution qexec = QueryExecutionFactory.create(query, jena);
 
 		SparqlSelectResponse result;
 		try {
@@ -104,9 +83,8 @@ public class EcConnectinManagerLocalTest {
 			qexec.close();
 		}
 
-		assertTrue(result.getResult().next().get("O").toString().equals("http://demo.play-project.eu/"));
-		assertTrue(result.getResult().next().get("O").toString().equals("VesselInformation"));
-		assertTrue(result.getResult().next().get("O").toString().equals("Clic2Call"));
+		assertTrue(result.getResult().next().get("O").toString()
+				.equals("Roland St\u00FChmer"));
 	}
 
 }
