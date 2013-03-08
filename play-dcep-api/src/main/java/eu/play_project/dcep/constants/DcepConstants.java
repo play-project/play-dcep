de7e8ea653c493536b93a744f89e36219eba5803
stuehmer <roland.stuehmer@fzi.de>
2013-03-08 12:05:24 +0100

    Update for constants (properties) handling.

--- play-dcep-api/src/main/java/eu/play_project/dcep/constants/DcepConstants.java
+++ play-dcep-api/src/main/java/eu/play_project/dcep/constants/DcepConstants.java
@@ -4,6 +4,14 @@ import java.util.Properties;
 
 import eu.play_project.play_commons.constants.Constants;
 
+/**
+ * Properties for DCEP. This class reads the file
+ * {@code play-dcep-distribution.properties} from the classpath and also has a
+ * default file included in the JAR. If a property is not found in these files
+ * the class further falls back to look in {@linkplain Constants}.
+ * 
+ * @author Roland Stühmer
+ */
 public class DcepConstants extends Constants {
 
 	private static Properties properties;
@@ -12,7 +20,7 @@ public class DcepConstants extends Constants {
 
 	public static Properties getProperties() {
 		if (properties == null) {
-			properties = Constants.getProperties(PROPERTIES,Constants.getProperties(PROPERTIES_DEFAULTS));
+			properties = Constants.getProperties(PROPERTIES,Constants.getProperties(PROPERTIES_DEFAULTS, Constants.getProperties()));
 		}
 		return properties;
 	}
