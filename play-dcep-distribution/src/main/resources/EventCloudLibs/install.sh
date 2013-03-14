03ec82bc866c0ebb2172b82b555d1dcba9ba57a8
stuehmer <roland.stuehmer@fzi.de>
2013-03-14 13:17:05 +0100

    Better versioning for transient SNAPSHOT.

--- play-dcep-distribution/src/main/resources/EventCloudLibs/install.sh
+++ play-dcep-distribution/src/main/resources/EventCloudLibs/install.sh
@@ -1,1 +1,2 @@
 mvn install:install-file -Dfile=mapdb-0.9-20121128.005552-13.jar -DgroupId=org.mapdb -DartifactId=mapdb -Dversion=0.9-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=mapdb-0.9-20121128.005552-13.jar -DgroupId=org.mapdb -DartifactId=mapdb -Dversion=0.9-20121128.005552-13 -Dpackaging=jar
