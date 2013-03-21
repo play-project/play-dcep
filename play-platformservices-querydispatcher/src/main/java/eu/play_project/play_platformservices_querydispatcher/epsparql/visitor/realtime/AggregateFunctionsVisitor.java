87bc97a6e1d4cc89d5ab1dec98c628d56cb9b257
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-03-21 17:46:00 +0100

    Merge.

---
+++ play-platformservices-querydispatcher/src/main/java/eu/play_project/play_platformservices_querydispatcher/epsparql/visitor/realtime/AggregateFunctionsVisitor.java
@@ -0,0 +1,17 @@
+package eu.play_project.play_platformservices_querydispatcher.epsparql.visitor.realtime;
+
+import com.hp.hpl.jena.sparql.expr.ExprAggregator;
+import com.hp.hpl.jena.sparql.expr.ExprFunction0;
+import com.hp.hpl.jena.sparql.expr.ExprFunction1;
+import com.hp.hpl.jena.sparql.expr.ExprFunction2;
+import com.hp.hpl.jena.sparql.expr.ExprFunction3;
+import com.hp.hpl.jena.sparql.expr.ExprFunctionN;
+import com.hp.hpl.jena.sparql.expr.ExprFunctionOp;
+import com.hp.hpl.jena.sparql.expr.ExprVar;
+import com.hp.hpl.jena.sparql.expr.ExprVisitor;
+import com.hp.hpl.jena.sparql.expr.NodeValue;
+
+public class AggregateFunctionsVisitor extends GenericVisitor{
+
+	
+}
