f91396074fbabce0fad1e73d338e63054a84a495
stuehmer <roland.stuehmer@fzi.de>
2013-03-25 15:43:51 +0100

    Documenting.

--- play-dcep-distributedetalis/src/main/java/eu/play_project/dcep/distributedetalis/api/VariableBindings.java
+++ play-dcep-distributedetalis/src/main/java/eu/play_project/dcep/distributedetalis/api/VariableBindings.java
@@ -3,6 +3,16 @@ package eu.play_project.dcep.distributedetalis.api;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Encapsulates value bindings which are incomplete. This functions as a restriction
+ * for the final values returned by a database request for historical data. The bindings
+ * must fulfill a join with the final data.
+ * Variable names (the keys in this collection) are specified using the SPARQL "?",
+ * e.g., {@code "?time"} instead of <strike>{@code "time"}</strike>.
+ * 
+ * @author Roland Stühmer
+ */
+
 public class VariableBindings extends HashMap<String, List<Object>> {
 	private static final long serialVersionUID = -7694752146628934047L;
 
--- play-platformservices-api/src/main/java/eu/play_project/play_platformservices/api/HistoricalData.java
+++ play-platformservices-api/src/main/java/eu/play_project/play_platformservices/api/HistoricalData.java
@@ -3,6 +3,13 @@ package eu.play_project.play_platformservices.api;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Encapsulates historical values for certain variables which were requested.
+ * Variable names (the keys in this collection) are specified without the SPARQL "?",
+ * e.g., {@code "time"} instead of <strike>{@code "?time"}</strike>.
+ * 
+ * @author Roland Stühmer
+ */
 public class HistoricalData extends HashMap<String, List<String>> {
 
 	private static final long serialVersionUID = -3304190292173439616L;
