06d6f252dcd548a7928fd09f6659d635098294f3
stuehmer <roland.stuehmer@fzi.de>
2013-03-26 15:13:42 +0100

    Use Exception as super type.

--- play-platformservices-api/src/main/java/eu/play_project/play_platformservices/api/QueryDispatchException.java
+++ play-platformservices-api/src/main/java/eu/play_project/play_platformservices/api/QueryDispatchException.java
@@ -1,6 +1,6 @@
 package eu.play_project.play_platformservices.api;
 
-public class QueryDispatchException extends Throwable {
+public class QueryDispatchException extends Exception {
 
 	private static final long serialVersionUID = -3053256787444130368L;
 
