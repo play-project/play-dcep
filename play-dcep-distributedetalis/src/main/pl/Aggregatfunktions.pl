0430cdc29b3d9ddf0f13c85b17eb724c5ab54ffb
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-03-28 09:42:32 +0100

    Bug fixes.

--- play-dcep-distributedetalis/src/main/pl/Aggregatfunktions.pl
+++ play-dcep-distributedetalis/src/main/pl/Aggregatfunktions.pl
@@ -6,17 +6,17 @@ addAgregatValue(Id, Element) :- (agregateListExists(Id) -> % Check if valuelist 
 		aggregatDb(Id,List), putInList(List,[],Element,Lnew), assert(aggregatDb(Id,Lnew)), retractall(aggregatDb(Id,List)); % Add element to existing list.
 		assert(aggregatDb(Id,[Element]))). % Put element in new list.
 
-calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, (Time-WindowSize), 0, 0, Avg)). %Calc avg recursivly
+calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, Id, (Time-WindowSize), 0, 0, Avg)). %Calc avg recursivly
 
 
 % Helpers
 agregateListExists(Id) :- (catch(aggregatDb(Id,_List), _Exception, false)). % Check if data structure exists.
 
-calcAvgIter([], _WindowEnd, Sum, N, Result):- ((Result is Sum/N)).
-calcAvgIter([H|T], WindowEnd, Sum, N, Result) :- (transformToNumber(H, Hn), transformToNumber(WindowEnd, WindowEndN), (Hn >= WindowEndN) ->
-			transformToNumber(H, Hn), calcAvgIter(T, WindowEnd, (Sum + Hn), (N + 1), Result);
-			transformToNumber(H, Hn), assert(aggregatDb(Id,H)), retractall(aggregatDb(Id,[H|T])), calcAvgIter([], WindowEnd, (Sum + Hn), (N + 1), Result)). % Stop recursion if value is out of window.
-
+calcAvgIter([], Id, _WindowEnd, Sum, N, Result):- ((Result is Sum/N)).
+calcAvgIter([H|T], Id, WindowEnd, Sum, N, Result) :- (transformToNumber(H, Hn), transformToNumber(WindowEnd, WindowEndN), (Hn >= WindowEndN) ->
+			transformToNumber(H, Hn), calcAvgIter(T, Id, WindowEnd, (Sum + Hn), (N + 1), Result);
+			transformToNumber(H, Hn),  calcAvgIter([], Id, WindowEnd, (Sum + Hn), (N + 1), Result)). % Stop recursion if value is out of window.
+%assert(aggregatDb(Id,H)), retractall(aggregatDb(Id,[H|T])),
 % Organize values in increasing order.
 putInList([],Left,Element, Result) :- (append(Left, [Element], Result)). % Element is last element in list. 
 putInList([H|T],LeftBuffer, Element, Result) :- ((transformToNumber(Element, ElementN), transformToNumber(H, Hn), ElementN>=Hn) -> 
