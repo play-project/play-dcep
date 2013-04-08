560cb254550403e63669683a635a47ada9bf8a53
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-09 01:11:48 +0200

    Basic idea of event triggered garbage collection.

--- play-dcep-distributedetalis/src/main/pl/ReferenceCounting.pl
+++ play-dcep-distributedetalis/src/main/pl/ReferenceCounting.pl
@@ -1,33 +1,23 @@
-% Increment counter for given ID.
-incrementReferenceCounter(ID):- (id(ID,X), X2 is X + 1,retractall(id(ID,X)), assert(id(ID,X2))).
+% Increment counter for given event id.
+incrementReferenceCounter(ID):- (id(ID, ID2, X), X2 is X + 1,retractall(id(ID, ID2, X)), assert(id(ID, ID2, X2))).
 
-% Decrement counter for given ID.
-decrementReferenceCounter(ID):- (id(ID,X), X2 is X - 1,retractall(id(ID,X)), assert(id(ID,X2))).
+% Decrement counter for given event id.
+decrementReferenceCounter(ID):- (id(ID, ID2, X), X2 is X - 1,retractall(id(ID, ID2, X)), assert(id(ID, ID2, X2))).
+
+% Represents the id of the last inserted event.
+%assert(lastsentEvent(0)).
 
 % Remove elements if counter == 0
-collectGarbage(ID) :- (id(ID,X), X<2, rdf_retractall(_S,_P,_O,ID), retractall(id(ID,X)); true).
+collectGarbage(ID) :- (id(ID,_ID2 ,X), X==0, rdf_retractall(_S,_P,_O,ID), retractall(id(ID, _ID2, X)); true).
 
-%Relational operator
-equal(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1=B1).
-notEqual(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1=\=B1).
-greater(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1>B1).
-greaterOrEqual(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1>=B1).
-less(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1<B1).
-lessOrEqual(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1=<B1).
-
-minus(A,B,Result) :- (transformToNumber(A,A1), transformToNumber(B,B1), (Result is A1-B1)).
-plus(A,B,Result) :- (transformToNumber(A,A1), transformToNumber(B,B1), (Result is A1+B1)).
-multiply(A,B,Result) :- (transformToNumber(A,A1), transformToNumber(B,B1), (Result is A1*B1)).
-
+%Delte unused triples.
+collectGarbage :- (forall(id(_ID, ID2, _X), ((lastsentEvent(C), (ID2<C)) -> (write('K ID2  '), write(ID2) , write('\n')); (write('G ID2  '), write(ID2), write('\n')) ))).
 
 transformToNumber(A, B):- catch(atom_number(A, B), _Exception, B is A).
-
-%Get abs value
-abs(InputValue,AbsValue):- (InputValue>=0, AbsValue is InputValue).
-abs(InputValue,AbsValue):- (InputValue<0, AbsValue is (InputValue*(-1))). 
 
 %Save variable values.
 variabeValuesAdd(PatternId, VariableName, Value):- (assert(variableValues(PatternId, VariableName,Value)); true).
 
 %Remove Variable value
 variabeValuesDel(PatternId, VariableName, Value):- retract(variableValues(PatternId, VariableName,Value)).
+
