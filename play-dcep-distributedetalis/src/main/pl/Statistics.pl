657e123ae11dddf92535cb90edef325f7001334b
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-14 06:09:33 +0200

    Add statistics source code.

---
+++ play-dcep-distributedetalis/src/main/pl/Statistics.pl
@@ -0,0 +1,16 @@
+% Print statistical data.
+
+% Print number of reference counters.
+printRefCountN :- (retractall(statClounter(_)), assert(statClounter(0)), forall(referenceCounter(DC, _, _), increment(DC) ), write('Number of reference counters: '), statClounter(N), write(N), nl).
+
+% Print number of events available in this instance.
+printNumberOfEvents :- (rdf_statistics(graphs(-Count)), write('Number of events: '),  write(Count), nl).
+
+% SEM-Web Lib statistics.
+printRdfStat :- write('Number of triples: '), rdf_statistics(triples(Count)), write(Count), nl.
+
+% Helpers
+%---------
+
+% Increment value in predicate statClounter\1.
+increment(_V) :- statClounter(N), (Np1 is N + 1), retractall(statClounter(_)), assert(statClounter(Np1)).
