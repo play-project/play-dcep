9e4147f3b647812bf7d40c593066d3bc6f7fd4af
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-15 11:17:39 +0200

    Aggregatefunction memory bug fix.

--- play-dcep-distributedetalis/src/main/pl/Aggregatfunktions.pl
+++ play-dcep-distributedetalis/src/main/pl/Aggregatfunktions.pl
@@ -3,10 +3,10 @@
 
 %Add new value to list.
 addAgregatValue(Id, Element) :- (agregateListExists(Id) -> % Check if valuelist exists.
-		aggregatDb(Id,List), putInList(List,[],Element,Lnew), assert(aggregatDb(Id,Lnew)), retractall(aggregatDb(Id,List)); % Add element to existing list.
+		aggregatDb(Id,List), putInList(List,[],Element,Lnew), retractall(aggregatDb(Id,List)), assert(aggregatDb(Id,Lnew)); % Add element to existing list.
 		assert(aggregatDb(Id,[Element]))). % Put element in new list.
 
-calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, Id, (Time-WindowSize), 0, 0, Avg)). %Calc avg recursivly
+calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, Id, (Time-WindowSize), 0, 0, Avg), retractall(aggregatDb(Id, _Dc))). %Calc avg recursivly
 
 
 % Helpers
@@ -14,18 +14,15 @@ agregateListExists(Id) :- (catch(aggregatDb(Id,_List), _Exception, false)). % Ch
 
 calcAvgIter([], Id, _WindowEnd, Sum, N, Result):- ((Result is Sum/N)).
 calcAvgIter([H|T], Id, WindowEnd, Sum, N, Result) :- (transformToNumber(H, Hn), transformToNumber(WindowEnd, WindowEndN), (Hn >= WindowEndN) ->
-			transformToNumber(H, Hn), calcAvgIter(T, Id, WindowEnd, (Sum + Hn), (N + 1), Result);
-			transformToNumber(H, Hn),  calcAvgIter([], Id, WindowEnd, (Sum + Hn), (N + 1), Result)). % Stop recursion if value is out of window.
+			calcAvgIter(T, Id, WindowEnd, (Sum + Hn), (N + 1), Result);
+			calcAvgIter([], Id, WindowEnd, (Sum + Hn), (N + 1), Result)). % Stop recursion if value is out of window.
 %assert(aggregatDb(Id,H)), retractall(aggregatDb(Id,[H|T])),
 % Organize values in increasing order.
 putInList([],Left,Element, Result) :- (append(Left, [Element], Result)). % Element is last element in list. 
 putInList([H|T],LeftBuffer, Element, Result) :- ((transformToNumber(Element, ElementN), transformToNumber(H, Hn), ElementN>=Hn) -> 
 	append(LeftBuffer, ([Element|([H|T])]),Result); %Put in front
-	append(LeftBuffer, [H], Left), (putInList(T, Left, Element, Result))). 
-
-
+	append(LeftBuffer, [H], Left), (putInList(T, Left, Element, Result))).
 %assert(aggregatDb(1,[])).
 
 %Debug versions.
 %calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, (Time-WindowSize), 0, 0, Avg), write('Average is: '), write(Avg), nl).
-
