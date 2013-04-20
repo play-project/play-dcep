e112560f4b546a8118ab2768d765bcd136000353
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-20 19:24:09 +0200

    Bug fix. Restart window.

--- play-dcep-distributedetalis/src/main/pl/Windows.pl
+++ play-dcep-distributedetalis/src/main/pl/Windows.pl
@@ -1,4 +1,5 @@
-% Time-based tumbling window.
-tumbling_window(PatternId, WindowTime):- alarm(WindowTime,  resetPattern(PatternId), _ID, []).
+% Time-based tumbling window. Reset pattern when widow time is up and restart timer.
+tumbling_window(PatternId, WindowTime):- alarm(WindowTime,  (resetPattern(PatternId), tumbling_window(PatternId, WindowTime)), _ID, []).
 
+% Delete all all consumed events for given pattern.
 resetPattern(PatternId) :- findall(etr_db(T2,T1,B,A,C,PatternId),(etr_db(T2,T1,B,A,C,PatternId),retract(etr_db(T2,T1,B,A,C,PatternId))),_L).
