304eca8338f1f8dc83dc7f0a5021e000d60f736c
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-27 18:22:39 +0200

    Unit test, retrieve types.

---
+++ play-platformservices-querydispatcher/src/test/java/eu/play_project/querydispatcher/epsparql/tests/TypeSystemTests.java
@@ -0,0 +1,62 @@
+package eu.play_project.querydispatcher.epsparql.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import eu.play_platform.platformservices.bdpl.VariableTypes;
+import eu.play_project.play_platformservices_querydispatcher.types.VariableTypeManager;
+
+public class TypeSystemTests {
+	
+	/**
+	 * Set different types and retrieve them.
+	 */
+	@Test
+	public void findType(){
+		VariableTypeManager vm = new VariableTypeManager();
+		
+		// Set types.
+		vm.addVariable("a", VariableTypes.CONSTRUCT_TYPE);
+		vm.addType("a", VariableTypes.AVG_TYPE);
+		
+		// Check if it is possible to retrieve informations.
+		assertTrue(vm.isType("a", VariableTypes.CONSTRUCT_TYPE));
+		assertTrue(vm.isType("a", VariableTypes.AVG_TYPE));
+		
+		assertFalse(vm.isType("a", VariableTypes.HISTORIC_TYPE));
+		assertFalse(vm.isType("a", VariableTypes.REALTIME_TYPE));
+		assertFalse(vm.isType("a", VariableTypes.MIN_TYPE));
+	}
+	
+	@Test
+	public void getAllVariablesOfOneType(){
+		VariableTypeManager vm = new VariableTypeManager();
+		
+		// Set types.
+		vm.addVariable("a", VariableTypes.CONSTRUCT_TYPE);
+		vm.addVariable("a", VariableTypes.REALTIME_TYPE);
+		vm.addType("a", VariableTypes.AVG_TYPE);
+		
+		vm.addVariable("b", VariableTypes.CONSTRUCT_TYPE);
+		vm.addVariable("b", VariableTypes.REALTIME_TYPE);
+		vm.addType("b", VariableTypes.MIN_TYPE);
+		
+		vm.addVariable("c", VariableTypes.CONSTRUCT_TYPE);
+		vm.addVariable("c", VariableTypes.REALTIME_TYPE);
+		vm.addType("c", VariableTypes.MAX_TYPE);
+		
+		List<String> vars = vm.getVariables(VariableTypes.REALTIME_TYPE);
+		assertTrue(vars.size()==3);
+		assertTrue(vars.contains("a"));
+		assertTrue(vars.contains("b"));
+		assertTrue(vars.contains("c"));
+		
+		vars = vm.getVariables(VariableTypes.MAX_TYPE);
+		assertTrue(vars.size()==1);
+		assertTrue(vars.contains("c"));	
+	}
+
+}
