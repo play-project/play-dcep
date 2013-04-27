267a83c34036c6e84ef92b1d2ffebb8162338af6
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-27 03:53:03 +0200

    Add sum aggregate method.

--- play-dcep-distributedetalis/src/main/pl/Aggregatfunktions.pl
+++ play-dcep-distributedetalis/src/main/pl/Aggregatfunktions.pl
@@ -6,7 +6,15 @@ addAgregatValue(Id, Element) :- (agregateListExists(Id) -> % Check if valuelist 
 		aggregatDb(Id,List), putInList(List,[],Element,Lnew), retractall(aggregatDb(Id,List)), assert(aggregatDb(Id,Lnew)); % Add element to existing list.
 		assert(aggregatDb(Id,[Element]))). % Put element in new list.
 
-calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, Id, (Time-WindowSize), 0, 0, Avg), retractall(aggregatDb(Id, _Dc))). %Calc avg recursivly
+% calcAverage(Id, WindowSize, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, Id, (Time-WindowSize), 0, 0, Avg), retractall(aggregatDb(Id, _Dc))). %Calc avg recursivly
+
+calcAverage(Id, WindowSize, Exp, Avg) :- (aggregatDb(Id,List),get_time(Time), calcAvgIter(List, Id, (Time-WindowSize), 0, 0, Avg), retractall(aggregatDb(Id, _Dc))). %Calc avg recursivly
+
+% Add value to sum with given Id.
+sumAdd(Id, Value):- (catch(sum(Id, V_old), E, assertSum(E, Id, Value)) -> % If value exists add new value. If not set value only.
+			(sum(Id, V_old), retract(sum(Id, V_old)), assert(sum(Id, (V_old + Value)))); true).
+assertSum(_E,Id, Value) :- (assert(sum(Id, Value)),false).
+
 
 % Safe value if it is the smallest value ever given.
 % Distinction between different sorages is done bay Id.
@@ -26,16 +34,18 @@ assertMaxVal(_E,Id, Value) :- (assert(maxValue(Id, Value)),false).
 % Helpers
 agregateListExists(Id) :- (catch(aggregatDb(Id,_List), _Exception, false)). % Check if data structure exists.
 
-calcAvgIter([], _Id, _WindowEnd, Sum, N, Result):- ((Result is (Sum/N))).
-calcAvgIter([H|T], Id, WindowEnd, Sum, N, Result) :- (transformToNumber(H, Hn), transformToNumber(WindowEnd, WindowEndN), (Hn >= WindowEndN) ->
+calcAvgIter([], _Id, Exp, _WindowEnd, Sum, N, Result):- ((Result is (Sum/N))).
+calcAvgIter([H|T], Id, Exp, WindowEnd, Sum, N, Result) :- (transformToNumber(H, Hn), transformToNumber(WindowEnd, WindowEndN), (Hn >= WindowEndN) ->
 			transformToNumber(H, Hn), calcAvgIter(T, Id, WindowEnd, (Sum + Hn), (N + 1), Result);
 			transformToNumber(H, Hn), calcAvgIter([], Id, WindowEnd, (Sum + Hn), (N + 1), Result)). % Stop recursion if value is out of window.
+ 
 %assert(aggregatDb(Id,H)), retractall(aggregatDb(Id,[H|T])),
 % Organize values in increasing order.
 putInList([],Left,Element, Result) :- (append(Left, [Element], Result)). % Element is last element in list. 
 putInList([H|T],LeftBuffer, Element, Result) :- ((transformToNumber(Element, ElementN), transformToNumber(H, Hn), ElementN>=Hn) -> 
 	append(LeftBuffer, ([Element|([H|T])]),Result); %Put in front
 	append(LeftBuffer, [H], Left), (putInList(T, Left, Element, Result))).
+
 
 
 
