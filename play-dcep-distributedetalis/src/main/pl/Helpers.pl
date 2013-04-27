612e67f5a4d1747d875e65b84d0231a45a379db7
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-27 05:02:51 +0200

    Define lib file for often used methods.

---
+++ play-dcep-distributedetalis/src/main/pl/Helpers.pl
@@ -0,0 +1,5 @@
+% This file contains methods with are use by other methods.
+% Author: Stefan Obermeier
+
+% Transform In to a number representation. In can be a Atom or number. The result is always a number.
+transformToNumber(In, Out):- catch(atom_number(In, Out), _Exception, Out is In).
