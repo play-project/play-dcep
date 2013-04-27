40abbd1ad439f937556edaed842648967c819baf
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-04-27 17:56:11 +0200

    Model variable types wit bitmap.

--- play-platformservices-bdpl/src/main/java/eu/play_platform/platformservices/bdpl/VariableTypes.java
+++ play-platformservices-bdpl/src/main/java/eu/play_platform/platformservices/bdpl/VariableTypes.java
@@ -1,15 +1,20 @@
 package eu.play_platform.platformservices.bdpl;
 /**
- * A variable can appear in historic,  realtime or both parts of an query.
- * They have different types to identify them.
+ * Variable can be of different types. Depending on how they are used.
+ * E.g. a variable can be used in event processing and in historic part.
+ * The types are represented as a bitmap.
+ * For this reason it is possible to combine different types and retrieve the types later.
  * 
  * @author sobermeier
  *
  */
-public enum VariableTypes {
+public class VariableTypes {
 
-	constructType,   // Variables from CONSTRUCT resultSet
-	realtimeType, // Variables from part which is processed by CEP engine
-	historicType,  // Variables form are filled by EventCloud
-	preloadType
+	public static final int CONSTRUCT_TYPE = 1 << 0;
+	public static final int REALTIME_TYPE  = 1 << 1;
+	public static final int HISTORIC_TYPE  = 1 << 2;
+	public static final int AVG_TYPE = 1 << 3;
+	public static final int MIN_TYPE = 1 << 4;
+	public static final int MAX_TYPE = 1 << 5;
+
 }
