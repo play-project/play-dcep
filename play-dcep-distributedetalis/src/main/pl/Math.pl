1a45246901bd6379e7d3e0ae9a8378ac9700c543
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-05-05 22:26:47 +0200

    Adapt math functions to new style.

--- play-dcep-distributedetalis/src/main/pl/Math.pl
+++ play-dcep-distributedetalis/src/main/pl/Math.pl
@@ -1,18 +1,84 @@
+% Math functions.
+%
 % This file contains common math operators.
-% Author: Stefan Obermeier
+% This functions are able to deal with integer and atom representation of numbers.
+%
+% @author Stefan Obermeier
 
 %Relational operator
-equal(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1=B1).
-notEqual(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1=\=B1).
-greater(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1>B1).
-greaterOrEqual(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1>=B1).
-less(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1<B1).
-lessOrEqual(A,B) :- (transformToNumber(A,A1), transformToNumber(B,B1), A1=<B1).
+equal(A, B) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	A1 = B1
+).
 
-minus(A,B,Result) :- (transformToNumber(A,A1), transformToNumber(B,B1), (Result is A1-B1)).
-plus(A,B,Result) :- (transformToNumber(A,A1), transformToNumber(B,B1), (Result is A1+B1)).
-multiply(A,B,Result) :- (transformToNumber(A,A1), transformToNumber(B,B1), (Result is A1*B1)).
+notEqual(A, B) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	A1 =\= B1
+).
+
+greater(A, B) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	A1 > B1
+).
+
+greaterOrEqual(A, B) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	A1 >= B1
+).
+
+less(A, B) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	A1 < B1
+).
+
+lessOrEqual(A, B) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	A1 =< B1
+).
+
+minus(A, B, Result) :- 
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	Result is (A1 - B1)
+).
+
+plus(A, B, Result) :-
+(
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	Result is (A1+B1)
+).
+
+multiply(A, B, Result) :- (
+	transformToNumber(A, A1),
+	transformToNumber(B, B1),
+	Result is (A1 * B1)
+).
 
 %Get abs value
-abs(InputValue,AbsValue):- (InputValue>=0, AbsValue is InputValue).
-abs(InputValue,AbsValue):- (InputValue<0, AbsValue is (InputValue*(-1))).
+abs(InputValue, AbsValue):- 
+(
+	transformToNumber(InputValue, InputValue1),
+	InputValue1 >= 0,
+	AbsValue is InputValue1
+).
+
+abs(InputValue1, AbsValue):- 
+(
+	transformToNumber(InputValue, InputValue1),
+	InputValue < 0,
+	AbsValue is (InputValue1*(-1))
+).
