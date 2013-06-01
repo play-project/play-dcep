a58db75e83e490d8a1fa895bb66b00e78e74a482
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-06-02 00:25:00 +0200

    Example how to get all subclasses of a given class. (SemWebLib)

--- play-dcep-distributedetalis/src/test/java/eu/play_project/dcep/distributedetalis/test/SemWebLibTests.java
+++ play-dcep-distributedetalis/src/test/java/eu/play_project/dcep/distributedetalis/test/SemWebLibTests.java
@@ -2,6 +2,10 @@ package eu.play_project.dcep.distributedetalis.test;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
 import org.junit.Test;
 
 import com.jtalis.core.JtalisContextImpl;
@@ -13,23 +17,37 @@ import eu.play_project.dcep.distributedetalis.PlayJplEngineWrapper;
 public class SemWebLibTests {
 	private JtalisContextImpl ctx;
 	
+	/**
+	 * Get all subclasses of a given class. Class hierarchy is loaded from file.
+	 */
 	@Test
-	public void rdfsSubclassExampleTest(){
+	public void rdfsSubclassExampleTest() throws IOException{
 		
-		PrologEngineWrapper<?> engine = PlayJplEngineWrapper.getPlayJplEngineWrapper();
+		PlayJplEngineWrapper engine = PlayJplEngineWrapper.getPlayJplEngineWrapper();
 		this.ctx = new JtalisContextImpl(engine);
 		
-		//Load libs.
-		ctx.getEngineWrapper().executeGoal("[library(semweb/rdf_db)]");
-		ctx.getEngineWrapper().executeGoal("[library(semweb/rdfs)]");
+		//Load sematic web libs.
+		engine.executeGoal("[library(semweb/rdf_db)]");
+		engine.executeGoal("[library(semweb/rdfs)]");
+
+		// Load class hierarchy.
+		//FIXME find a method to load it in java.
+		ctx.getEngineWrapper().executeGoal("rdf_load('" + new File (".").getCanonicalPath().replace("\\", "/") + "/src/test/resources/event_types.xml')");
 		
+		// Get all Telco subclasses.
+		Hashtable<String, Object>[] result =  engine.execute("rdf(X, 'http://www.w3.org/2000/01/rdf-schema#subClassOf', 'http://events.event-processing.org/types/Telco').");
 		
-		ctx.getEngineWrapper().executeGoal("rdf_assert('MeasurementEvent', rdfs:subClassOf, 'Event')");
-		ctx.getEngineWrapper().executeGoal("rdf_assert('PrecipitationEvent', rdfs:subClassOf, 'MeasurementEvent')");
-		boolean result = ctx.getEngineWrapper().executeGoal("rdfs_subclass_of('PrecipitationEvent', 'Event'");
+		String[] expectedClasses = {"'http://events.event-processing.org/types/ContextualizedLatitudeEvent'","'http://events.event-processing.org/types/UcTelcoAnswer'","'http://events.event-processing.org/types/UcTelcoAvailability'","'http://events.event-processing.org/types/UcTelcoCall'","'http://events.event-processing.org/types/UcTelcoClic2Call'","'http://events.event-processing.org/types/UcTelcoEsrRecom'","'http://events.event-processing.org/types/UcTelcoGeoLocation'","'http://events.event-processing.org/types/UcTelcoOutNetwork'","'http://events.event-processing.org/types/UcTelcoPresence'","'http://events.event-processing.org/types/UcTelcoSmsCustomerAlert'","'http://events.event-processing.org/types/UcTelcoTrafficJam'","'http://events.event-processing.org/types/UcTelcoUnexpected'"};
 		
-		assertTrue(result);
-		
+		// Compare result wit expected result.
+		for (int i = 0; i < result.length; i++) {
+			for (int j = 0; j < result[i].get("X").toString().getBytes().length; j++) {
+				if(result[i].get("X").toString().getBytes()[j] != expectedClasses[i].getBytes()[j] ){
+					fail();
+				}
+			}
+			
+		}
 		
 	}
 
