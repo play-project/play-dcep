9191cc582e27697dcb0b382b1c4fe5c9c79effaf
Stefan Obermeier <stefan.obermeier@fzi.de>
2013-07-07 00:30:40 +0200

    Simple distributed version. Queries are distributed in in a round robin fashion to all instances defined in DcepManager.

---
+++ play-dcep-core/src/main/java/eu/play_project/dcep/DcepManager.java
@@ -0,0 +1,70 @@
+package eu.play_project.dcep;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.NamingException;
+
+import org.objectweb.fractal.api.NoSuchInterfaceException;
+import org.objectweb.proactive.core.component.Fractive;
+import org.objectweb.proactive.core.component.representative.PAComponentRepresentative;
+import org.objectweb.proactive.core.util.URIBuilder;
+
+import eu.play_project.dcep.api.DcepManagmentApi;
+
+/**
+ * Manage dEtalis instances.
+ * @author sobermeier
+ *
+ */
+public class DcepManager {
+	List<PAComponentRepresentative>  dEtalis; // Mapping between instance name and instance.
+	int lastUsedNode;
+	
+	DcepManager(){
+		dEtalis = new LinkedList<PAComponentRepresentative>();
+	}
+	
+	/**
+	 * Instantiate dEtalises.
+	 */
+	public void init() {
+		//TODO read deployment from file or ...
+		String destinations[] = {"127.0.0.1"};
+		
+		for (int i = 0; i < destinations.length; i++) {
+			try {
+				dEtalis.add(createInstance("dEtalis", destinations[0]));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+		}	
+	}
+	
+	private PAComponentRepresentative createInstance(String name, String host) throws IOException, NamingException{
+		return Fractive.lookup(URIBuilder.buildURI(host, name, "rmi", 1099).toString());
+	}
+	
+	/**
+	 * Get DcepManagmentApi from one instance after the other.
+	 * @return Proxy to dEtalis instance.
+	 */
+	public DcepManagmentApi getManagementApi(){
+		lastUsedNode++;
+		try {
+			return (DcepManagmentApi)dEtalis.get(lastUsedNode%dEtalis.size()).getFcInterface("DcepManagmentApi");
+		} catch (NoSuchInterfaceException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
+	
+
+
+
