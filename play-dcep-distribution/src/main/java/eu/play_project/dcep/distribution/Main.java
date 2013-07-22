100482975aae42dee531c351e84bc03bc5bd34a2
stuehmer <roland.stuehmer@fzi.de>
2013-07-22 14:13:32 +0200

    Reorganize project for maven appassembler

--- play-dcep-distribution/src/main/java/eu/play_project/dcep/distribution/Main.java
+++ play-dcep-distribution/src/main/java/eu/play_project/dcep/distribution/Main.java
@@ -1,13 +1,9 @@
 package eu.play_project.dcep.distribution;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 
+import org.apache.commons.io.IOUtils;
 import org.etsi.uri.gcm.util.GCM;
-import org.objectweb.fractal.adl.ADLException;
 import org.objectweb.fractal.adl.Factory;
 import org.objectweb.fractal.api.Component;
 import org.objectweb.fractal.api.NoSuchInterfaceException;
@@ -27,117 +23,125 @@ public class Main {
 
 	private static final String propertiesFile = "proactive.java.policy";
 	private static Logger logger;
+	private Component root;
 
+	/**
+	 * Start DCEP. There are three ways to stop it: (1) by killing the main
+	 * thread a shutdownhook is caught (2) by sending 3 newlines on stdin (3)
+	 * when an exception happens during start.
+	 */
 	public static void main(String[] args) {
-		logger = LoggerFactory.getLogger(Main.class);
-		Component root = null;
-		
+		final Main main = new Main();
+
 		try {
+			main.start();
 			
-			/*
-			 * Set up Components
-			 */
-			CentralPAPropertyRepository.JAVA_SECURITY_POLICY.setValue(propertiesFile);
-			CentralPAPropertyRepository.GCM_PROVIDER.setValue("org.objectweb.proactive.core.component.Fractive");
-	
-			Factory factory = FactoryFactory.getFactory();
-			HashMap<String, Object> context = new HashMap<String, Object>();
-	
-			root = (Component) factory.newComponent("PlayPlatform", context);
-	
-			GCM.getGCMLifeCycleController(root).startFc();
-	
-			boolean init = false;
-			
-			// Wait for all subcomponents to be started
-			while (!init) {
-				boolean overallInitStatus = true;
-				for(Component subcomponent : GCM.getContentController(root).getFcSubComponents()){
-					overallInitStatus = overallInitStatus && GCM.getGCMLifeCycleController(subcomponent).getFcState().equals(LifeCycleController.STARTED);
-				}
-				if (overallInitStatus == true) {
-					init = true;
-				}
-				else {
-					logger.info("Wait for all subcomponents to be started...");
-				}
-				Thread.sleep(500);
-			}
-					
-			
-			// Get interfaces
-			QueryDispatchApi queryDispatchApi = ((eu.play_project.play_platformservices.api.QueryDispatchApi) root.getFcInterface("QueryDispatchApi"));
-	
-			/*
-			 *  Compile and Deploy Queries
-			 */
-			for (String queryFileName : DcepConstants.getProperties().getProperty("dcep.startup.registerqueries").split(",")) {
-				queryFileName = queryFileName.trim();
-				try {
-					String	queryString = getSparqlQueries(queryFileName);
-					logger.info(queryString);
-					queryDispatchApi.registerQuery(queryFileName, queryString);
-				} catch (QueryDispatchException e) {
-					logger.warn("Error registering query {} on startup: {}", queryFileName, e.getMessage());
-				} catch (NullPointerException e) {
-					logger.warn("Error registering query {} on startup: {}", queryFileName, e.getMessage());
-				}
-			}
-			
+			Runtime.getRuntime().addShutdownHook(new Thread(){
+			    @Override
+				public void run() {
+			        logger.debug("Shutdown hook was invoked. Shutting down...");
+			        try {
+						main.stop();
+					} catch (Exception e) {
+						logger.info(e.getMessage());
+					}
+			    }
+			});
+						
 			System.out.println("Press 3x RETURN to shutdown the application");
 			System.in.read();
 			System.in.read();
 			System.in.read();
 			
-		} catch (IOException e){
-			logger.error(e.getMessage(), e);
-		} catch (InterruptedException e) {
-			logger.error(e.getMessage(), e);
-		} catch (NoSuchInterfaceException e) {
-			logger.error(e.getMessage(), e);
-		} catch (IllegalLifeCycleException e) {
-			logger.error(e.getMessage(), e);
-		} catch (ADLException e) {
+			main.stop();
+			
+		} catch (Exception e){
 			logger.error(e.getMessage(), e);
 		} finally {
 			try {
-				// Stop and terminate GCM Components
-				logger.info("Terminate application");
-				logger.trace("Send stopFc to all components");
-
-				if (root != null) {
-					
-					// Stop is recursive...
-					GCM.getGCMLifeCycleController(root).stopFc();
-					// Terminate is not recursive:
-					for(Component subcomponent : GCM.getContentController(root).getFcSubComponents()){
-						GCM.getGCMLifeCycleController(subcomponent).terminateGCMComponent();
-					}
-					GCM.getGCMLifeCycleController(root).terminateGCMComponent();
-				}
-				
-			} catch (IllegalLifeCycleException e) {
-				logger.error(e.getMessage(), e);
-			} catch (NoSuchInterfaceException e) {
+				main.stop();
+			} catch (Exception e) {
 				logger.error(e.getMessage(), e);
 			}
 		}
 
 	}
-	
-	private static String getSparqlQueries(String queryFile) throws IOException {
-		InputStream is = Main.class.getClassLoader().getResourceAsStream(queryFile);
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		StringBuffer sb = new StringBuffer();
-		String line;
-		
-		while (null != (line = br.readLine())) {
-				sb.append(line);
-				sb.append("\n");
-		}
-		br.close();
-		is.close();
 
-		return sb.toString();
+	public void start() throws Exception {
+		logger = LoggerFactory.getLogger(Main.class);
+
+		/*
+		 * Set up Components
+		 */
+		CentralPAPropertyRepository.JAVA_SECURITY_POLICY.setValue(propertiesFile);
+		CentralPAPropertyRepository.GCM_PROVIDER.setValue("org.objectweb.proactive.core.component.Fractive");
+
+		Factory factory = FactoryFactory.getFactory();
+		HashMap<String, Object> context = new HashMap<String, Object>();
+
+		root = (Component) factory.newComponent("PlayPlatform", context);
+
+		GCM.getGCMLifeCycleController(root).startFc();
+
+		boolean init = false;
+		
+		// Wait for all subcomponents to be started
+		while (!init) {
+			boolean overallInitStatus = true;
+			for(Component subcomponent : GCM.getContentController(root).getFcSubComponents()){
+				overallInitStatus = overallInitStatus && GCM.getGCMLifeCycleController(subcomponent).getFcState().equals(LifeCycleController.STARTED);
+			}
+			if (overallInitStatus == true) {
+				init = true;
+			}
+			else {
+				logger.info("Wait for all subcomponents to be started...");
+			}
+			Thread.sleep(500);
+		}
+				
+		
+		// Get interfaces
+		QueryDispatchApi queryDispatchApi = ((eu.play_project.play_platformservices.api.QueryDispatchApi) root.getFcInterface("QueryDispatchApi"));
+
+		/*
+		 *  Compile and Deploy Queries
+		 */
+		for (String queryFileName : DcepConstants.getProperties().getProperty("dcep.startup.registerqueries").split(",")) {
+			queryFileName = queryFileName.trim();
+			try {
+				String	queryString = IOUtils.toString(Main.class.getClassLoader().getResourceAsStream(queryFileName));
+				logger.info(queryString);
+				queryDispatchApi.registerQuery(queryFileName, queryString);
+			} catch (QueryDispatchException e) {
+				logger.warn("Error registering query {} on startup: {}", queryFileName, e.getMessage());
+			}
+		}
+	}
+
+	public void stop() throws Exception {
+		try {
+			// Stop and terminate GCM Components
+			logger.info("Terminate application");
+			logger.trace("Send stopFc to all components");
+
+			if (root != null) {
+				
+				// Stop is recursive...
+				GCM.getGCMLifeCycleController(root).stopFc();
+				// Terminate is not recursive:
+				for(Component subcomponent : GCM.getContentController(root).getFcSubComponents()){
+					GCM.getGCMLifeCycleController(subcomponent).terminateGCMComponent();
+				}
+				GCM.getGCMLifeCycleController(root).terminateGCMComponent();
+				
+				//TODO stuehmer: use root.join(2000) to wait for shutdown
+			}
+			
+		} catch (IllegalLifeCycleException e) {
+			logger.error(e.getMessage());
+		} catch (NoSuchInterfaceException e) {
+			logger.error(e.getMessage());
+		}
 	}
 }
