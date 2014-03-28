b10aada063a9a3b0967592ad3ef70ea54c15bb39
Stefan Obermeier <stefan.obermeier@fzi.de>
2014-03-28 14:09:02 +0100

    Move filter out of named graph braces.

--- play-platformservices-querydispatcher/src/main/java/com/hp/hpl/jena/sparql/serializer/HistoricalGraphFormaterElement.java
+++ play-platformservices-querydispatcher/src/main/java/com/hp/hpl/jena/sparql/serializer/HistoricalGraphFormaterElement.java
@@ -8,6 +8,7 @@ import org.apache.jena.atlas.io.IndentedWriter;
 
 import com.hp.hpl.jena.sparql.core.PathBlock;
 import com.hp.hpl.jena.sparql.core.TriplePath;
+import com.hp.hpl.jena.sparql.syntax.ElementFilter;
 import com.hp.hpl.jena.sparql.syntax.ElementNamedGraph;
 import com.hp.hpl.jena.sparql.syntax.ElementPathBlock;
 import com.hp.hpl.jena.sparql.syntax.ElementService;
@@ -17,6 +18,8 @@ import eu.play_project.play_platformservices_querydispatcher.historicalQuery.Det
 public class HistoricalGraphFormaterElement extends FormatterElement {
 
 	Map<String, String> historicalCloudQueries = new HashMap<String, String>();
+	IndentedLineBuffer filterExp; // Represent filter in a named graph pattern.
+	
 
 	public HistoricalGraphFormaterElement(IndentedWriter out, SerializationContext context) {
 		super(out, context);
@@ -27,30 +30,42 @@ public class HistoricalGraphFormaterElement extends FormatterElement {
 
 		// PLAY: Replace output buffer temporally to get one graph only
 		IndentedWriter original = super.out;
-
 		super.out = new IndentedLineBuffer();
-
 
 		DetectCloudId.resetBuffer();
 		
 		// Process elements.
+		filterExp = new IndentedLineBuffer();
 		visitNodePattern("GRAPH", el.getGraphNameNode(), el.getElement());
 
+		// Add filter after GRAPH{} 
+		
+		System.out.println("\n\n\n\n " + out.toString().substring(0, out.toString().length()));
+		
 		// Save cloud ID and the corresponding query.
-		//if( DetectCloudId.getState() == State.FOUND ) {
-			if (historicalCloudQueries.containsKey(DetectCloudId.getCloudId())) {
-				historicalCloudQueries.put(DetectCloudId.getCloudId(),
-				historicalCloudQueries.get(DetectCloudId.getCloudId()) + "\n" + out.toString());
-			} else {
-				historicalCloudQueries.put(DetectCloudId.getCloudId(), out.toString());
-			}
-		//}
+		if (historicalCloudQueries.containsKey(DetectCloudId.getCloudId())) {
+			historicalCloudQueries.put(DetectCloudId.getCloudId(),
+			historicalCloudQueries.get(DetectCloudId.getCloudId()) + "\n" + out.toString() + "\n  " + filterExp);
+		} else {
+			historicalCloudQueries.put(DetectCloudId.getCloudId(), out.toString() + "\n  " + filterExp);
+		}
 
 		// Continue with old buffer.
 		super.out = original;
-
 	}
-
+	
+	@Override
+	public void visit(ElementFilter el) {
+		// PLAY: Replace output buffer temporally to get filter exp in own var.
+		IndentedWriter original = super.out;
+		super.out = filterExp;
+		
+		super.visit(el);
+		
+		// Continue with old buffer.
+		super.out = original;
+	}
+	
 	@Override
 	public void visit(ElementPathBlock el) {
 
