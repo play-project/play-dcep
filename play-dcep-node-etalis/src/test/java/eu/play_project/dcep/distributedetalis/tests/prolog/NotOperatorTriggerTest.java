e9d4a94dba6a167f8dd2e2a51d60a6ef706f2ae2
Stefan Obermeier <stefan.obermeier@fzi.de>
2014-05-12 10:16:28 +0200

    Extend NotOperatorTriggerTest.

--- play-dcep-node-etalis/src/test/java/eu/play_project/dcep/distributedetalis/tests/prolog/NotOperatorTriggerTest.java
+++ play-dcep-node-etalis/src/test/java/eu/play_project/dcep/distributedetalis/tests/prolog/NotOperatorTriggerTest.java
@@ -12,63 +12,50 @@ import com.jtalis.core.JtalisContext;
 import com.jtalis.core.JtalisContextImpl;
 import com.jtalis.core.event.AbstractJtalisEventProvider;
 import com.jtalis.core.event.EtalisEvent;
-import com.jtalis.core.plengine.JPLEngineWrapper;
-import com.jtalis.core.plengine.PrologEngineWrapper;
+import eu.play_project.dcep.distributedetalis.PlayJplEngineWrapper;
+import eu.play_project.dcep.distributedetalis.configurations.helpers.LoadPrologCode;
 
 public class NotOperatorTriggerTest {
 	
 	@Test
-	public void testCnotOperator() throws InterruptedException {
+	public void testEventTrigger() throws InterruptedException {
 		try {
 			long delay = 100;
-			final List<EtalisEvent> list = new LinkedList<EtalisEvent>();
+			final List<EtalisEvent> complexEventList = new LinkedList<EtalisEvent>();
 
-			PrologEngineWrapper<?> engine = new JPLEngineWrapper();
+			PlayJplEngineWrapper engine = PlayJplEngineWrapper.getPlayJplEngineWrapper();
 			JtalisContext context = new JtalisContextImpl(engine);
 			context.addEventTrigger("c");
 
 			context.registerOutputProvider(new AbstractJtalisEventProvider() {
 				@Override
 				public void outputEvent(EtalisEvent event) {
-					list.add(event);
+					complexEventList.add(event);
 				}
 			});
+			
+			// Load prolog libs.
+			LoadPrologCode cl = new LoadPrologCode();
+			cl.loadCode("TimeTrigger.pl", engine);
 		    	 
-			context.addDynamicRule("c <- (a seq b seq t) 'cnot' d");
-			//context.pushEvent(new EtalisEvent("b"));
+			context.addDynamicRule("c <- (a seq b) 'cnot' d");
 			
-			// Event d appears after seq.
+			// Fulfill pattern.
 			context.pushEvent(new EtalisEvent("a"));
 			context.pushEvent(new EtalisEvent("b"));
-			context.pushEvent(new EtalisEvent("e"));
-			context.pushEvent(new EtalisEvent("d"));
 			Thread.sleep(delay);
-			Assert.assertEquals(1, list.size());
+			Assert.assertEquals(1, complexEventList.size());
 			
-			// Event 'd' appeared. For this reason no 'c' was produced.
+			// Event b appears  in the period between event a and b.
 			context.pushEvent(new EtalisEvent("a"));
-			context.pushEvent(new EtalisEvent("d"));
+			// Event d will be sent after one second.
+			engine.executeGoal("triggerEventWithDelay(d, 1)");
+			Thread.sleep(1020);
+			engine.executeGoal("write('')");
 			context.pushEvent(new EtalisEvent("b"));
-			context.pushEvent(new EtalisEvent("e"));
 			Thread.sleep(delay);
-			Assert.assertEquals(1, list.size());
 			
-			// No 'b' appeared. For this reason 'c' was produced.
-			context.pushEvent(new EtalisEvent("a"));
-			context.pushEvent(new EtalisEvent("b"));
-			context.pushEvent(new EtalisEvent("e"));
-			Thread.sleep(delay);
-			Assert.assertEquals(2, list.size());
-			
-			// No 'd' appeared before seq. For this reason no 'c' was produced.
-			context.pushEvent(new EtalisEvent("d"));
-			context.pushEvent(new EtalisEvent("a"));
-			context.pushEvent(new EtalisEvent("b"));
-			context.pushEvent(new EtalisEvent("e"));
-			Thread.sleep(delay);
-			Assert.assertEquals(3, list.size());
-			
-			Thread.sleep(delay); // wait a little bit for the events to be processed
+			Assert.assertEquals(1, complexEventList.size());
 		}
 		catch (Exception e) {
 			Assert.fail(e.getMessage());
