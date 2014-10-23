9aa487a6e84e9bc084256905ee871d6d53c5f2b3
ningyuan <ny_pan@hotmail.com>
2014-10-23 22:30:20 +0200

    Continue: define dynamic array through json

--- play-platformservices-querydispatcher-esper/src/main/java/eu/play_project/platformservices/querydispatcher/query/extension/function/implement/JSONTOARRAY.java
+++ play-platformservices-querydispatcher-esper/src/main/java/eu/play_project/platformservices/querydispatcher/query/extension/function/implement/JSONTOARRAY.java
@@ -4,6 +4,7 @@
 package eu.play_project.platformservices.querydispatcher.query.extension.function.implement;
 
 import org.openrdf.model.Literal;
+import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.impl.ValueFactoryImpl;
@@ -11,6 +12,8 @@ import org.openrdf.rio.ntriples.NTriplesUtil;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+
+import eu.play_project.platformservices.bdpl.parser.util.BDPLConstants;
 
 /**
  * @author ningyuan 
@@ -24,36 +27,91 @@ public class JSONTOARRAY {
 		String [][][] ret = null;
 		Gson gson = new Gson();
 		
+		String [][] mArray = null;
 		try{
-			String [] array = gson.fromJson(json, String[].class);
+			mArray = gson.fromJson(json, String[][].class);
+		}catch(JsonSyntaxException e){}
+		
+		if(mArray != null){
 			ValueFactory vf = new ValueFactoryImpl();
-			//XXX only support one dimension
-			ret = new String[array.length][1][2];
-				System.out.println("Json to array:");
-			for(int i = 0; i < array.length; i++){
-				try{
-					Value v = NTriplesUtil.parseValue(array[i], vf);
+			
+			if(mArray.length > 0){
+				int dimension = mArray[0].length;
+				ret = new String[mArray.length][dimension][3];
 					
-					if(v instanceof Literal){
-						ret[i][0][0] = ((Literal) v).getLabel();
-					}
-					else{
-						ret[i][0][0] = v.toString();
-					}
+				for(int i = 0; i < mArray.length; i++){
+					for(int j = 0; j < dimension; j++){
+						try{
+							Value v = NTriplesUtil.parseValue(mArray[i][j], vf);
+							
+							if(v instanceof Literal){
+								ret[i][j][0] = String.valueOf(BDPLConstants.TYPE_LITERAL);
+								ret[i][j][1] = ((Literal) v).getLabel();
+								ret[i][j][2] = v.toString();
+							}
+							else if(v instanceof URI){
+								ret[i][j][0] = String.valueOf(BDPLConstants.TYPE_IRI);
+								ret[i][j][1] = v.toString();
+							}
+							else{
+								ret[i][j][0] = String.valueOf(BDPLConstants.TYPE_UNKNOWN);
+								ret[i][j][1] = v.toString();
+							}
+						}
+						catch(IllegalArgumentException e){
+							ret[i][j][0] = String.valueOf(BDPLConstants.TYPE_UNKNOWN);
+							ret[i][j][1] = mArray[i][j];
+						}
 						
-					ret[i][0][1] = v.toString();
+							System.out.print(ret[i][j][1]+" ");
+					}
+						System.out.println();
 				}
-				catch(IllegalArgumentException e){
-					ret[i][0][0] = array[i];
-					ret[i][0][1] = array[i];
-				}
-					System.out.print(ret[i][0][1]+"   "+ret[i][0][0]+";  ");
+					System.out.println();
 			}
-				System.out.println();
 		}
-		catch(JsonSyntaxException e){
-			return null;
+		else{
+			String [] array = null;
+			try{
+				array = gson.fromJson(json, String[].class);
+			}catch(JsonSyntaxException e){}
+			
+			if(array != null){
+				ValueFactory vf = new ValueFactoryImpl();
+				
+				if(array.length > 0){
+					
+					ret = new String[array.length][1][3];
+						
+					for(int i = 0; i < array.length; i++){
+						try{
+							Value v = NTriplesUtil.parseValue(array[i], vf);
+								
+							if(v instanceof Literal){
+								ret[i][0][0] = String.valueOf(BDPLConstants.TYPE_LITERAL);
+								ret[i][0][1] = ((Literal) v).getLabel();
+								ret[i][0][2] = v.toString();
+							}
+							else if(v instanceof URI){
+								ret[i][0][0] = String.valueOf(BDPLConstants.TYPE_IRI);
+								ret[i][0][1] = v.toString();
+							}
+							else{
+								ret[i][0][0] = String.valueOf(BDPLConstants.TYPE_UNKNOWN);
+								ret[i][0][1] = v.toString();
+							}
+						}
+						catch(IllegalArgumentException e){
+							ret[i][0][0] = String.valueOf(BDPLConstants.TYPE_UNKNOWN);
+							ret[i][0][1] = array[i];
+						}
+							System.out.print(ret[i][0][1]+" ");
+					}
+						System.out.println();
+				}
+			}
 		}
+		
 		return ret;
 	}
 	
